ffb86f00887cedfd5ad09080598f1f42f5cfb644
Claudio Giordano <claudio.giordano@autistici.org>
2016-09-04 21:07:23 +0200

    Added interface methods

--- src/LoggerInterface.php
+++ src/LoggerInterface.php
@@ -8,5 +8,19 @@ namespace clagiordano\weblibs\logger;
  */
 interface LoggerInterface
 {
+    public function emergency();
 
+    public function alert();
+
+    public function critical();
+
+    public function error();
+
+    public function warning();
+
+    public function notice();
+
+    public function info();
+
+    public function debug();
 }
