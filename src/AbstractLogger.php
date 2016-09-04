1fdd6ab99ab4888a6d02e145c764959c99252b8c
Claudio Giordano <claudio.giordano@autistici.org>
2016-09-04 21:13:19 +0200

    Added class relations

--- src/AbstractLogger.php
+++ src/AbstractLogger.php
@@ -3,10 +3,73 @@
 namespace clagiordano\weblibs\logger;
 
 /**
- * Class AbstractLogger
+ * Class AbstractLogger, abstract logger methods
  * @package clagiordano\weblibs\logger
  */
-class AbstractLogger
+abstract class AbstractLogger implements LoggerInterface
 {
+    /**
+     * @return mixed
+     */
+    public function emergency()
+    {
+        // TODO: Implement emergency() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function alert()
+    {
+        // TODO: Implement alert() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function critical()
+    {
+        // TODO: Implement critical() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function error()
+    {
+        // TODO: Implement error() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function warning()
+    {
+        // TODO: Implement warning() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function notice()
+    {
+        // TODO: Implement notice() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function info()
+    {
+        // TODO: Implement info() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function debug()
+    {
+        // TODO: Implement debug() method.
+    }
 
 }
